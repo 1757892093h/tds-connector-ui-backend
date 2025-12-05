0cc01ef3af8ec29372809769dc5b2f3f8d7c9bd7
wjh <86991015+1757892093h@users.noreply.github.com>
2025-12-05 16:45:58 +0800

    补充注册了其他路由函数

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from .config import settings
-from .routers import auth, identity, offerings, contracts
+from .routers import auth, identity, offerings, contracts,policy_templates, contract_templates, data_requests
 
 app = FastAPI(title=settings.app_name)
 
@@ -9,7 +9,9 @@ app.include_router(auth.router)
 app.include_router(identity.router)
 app.include_router(offerings.router)
 app.include_router(contracts.router)
-
+app.include_router(policy_templates.router)
+app.include_router(contract_templates.router)
+app.include_router(data_requests.router)
 
 @app.get("/")
 async def root():
